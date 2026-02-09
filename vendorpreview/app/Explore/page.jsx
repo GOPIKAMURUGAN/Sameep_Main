0f34eae2eb7b82bd35a428bd6ba8ff60d1f53937
Naresh Ranga <nareshranga@Nareshs-MacBook-Air.local>
2026-02-09 16:09:17 +0530

    Disable static export for HarishPreview pages

--- vendorpreview/app/Explore/page.jsx
+++ vendorpreview/app/Explore/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
-
+export const revalidate = 0;
 
 import AdvantageSection from "../About/About";
 import RootsSection from "../Root/RootSection";
