363889980d1d35bb12bc04d3956fb5517a4582c2
Naresh Ranga <nareshranga@Nareshs-MacBook-Air.local>
2026-02-09 21:18:16 +0530

    Make VendorTitleUpdater Suspense-safe

--- vendorpreview/app/VendorTitleUpdater.jsx
+++ vendorpreview/app/VendorTitleUpdater.jsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, use } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function VendorTitleUpdater() {
-  const searchParams = useSearchParams();
+  // ⭐ Make useSearchParams Suspense-safe
+  const searchParams = use(useSearchParams());
+
   const vendorName = searchParams.get("vendorName");
 
   useEffect(() => {
