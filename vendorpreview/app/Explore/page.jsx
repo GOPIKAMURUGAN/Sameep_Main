2dfd1007f02f2d98529ea0ca93295dfcc9b5153a
Naresh Ranga <nareshranga@Nareshs-MacBook-Air.local>
2026-02-09 16:14:51 +0530

    Fix Next.js build: remove revalidate from client components

--- vendorpreview/app/Explore/page.jsx
+++ vendorpreview/app/Explore/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 import AdvantageSection from "../About/About";
 import RootsSection from "../Root/RootSection";
